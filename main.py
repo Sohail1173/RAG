117f6cc08c6e6d78ffe8fc8264b7bf73ae5d2337
Sohail1173 <mdsohailahmed711@gmail.com>
2024-03-30 23:24:29 +0530

    completed

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import JSONLoader
+import json
+from pathlib import Path
+from pprint import pprint
 import os
 
 st.set_page_config(page_title="Document Genie",layout="wide")
@@ -21,19 +25,21 @@ def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
         pdf_reader=PdfReader(pdf)
-        for page in pdf_reader.get_pages:
+        for page in pdf_reader.pages:
             text+=page.extract_text()
     return text
 
 
+
+
 def get_text_chunks(text):
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=1000)
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=10000)
     chunks=text_splitter.split_text(text)
     return chunks
 
 def get_vector_store(text_chunks,api_key):
-    embeddings=GoogleGenerativeAIEmbeddings(model="models/embeddings-001",google_api_key=api_key)
-    vector_store=FAISS.from_texts(text_chunks,embeddings=embeddings)
+    embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key)
+    vector_store=FAISS.from_texts(text_chunks,embedding=embeddings)
     vector_store.save_local("faiss_index")
 
 
@@ -49,16 +55,16 @@ def get_conversational_chain():
 
     Answer:
     """
-    model=ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3,google_api_ey=api_key)
-    prompt=PromptTemplate(tempate=prompt_template,input_variables=["context","question"])
+    model=ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3,google_api_key=api_key)
+    prompt=PromptTemplate(template=prompt_template,input_variables=["context","question"])
     chain=load_qa_chain(model,chain_type="stuff",prompt=prompt)
     return chain
 
 
 
 def user_input(user_question,api_key):
-    embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-0.001",goggle_api_key=api_key)
-    new_db=False.load_local("faiss_index",embedding)
+    embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key)
+    new_db=FAISS.load_local("faiss_index",embedding,allow_dangerous_deserialization=True)
     docs=new_db.similarity_search(user_question)
     chain=get_conversational_chain()
     response=chain({"input_documents":docs,"question":user_question},return_only_outputs=True)
@@ -75,10 +81,13 @@ def main():
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs=st.file_uploader("Upload your PDF Files and Click on the Submit And process Button",accept_multiple=True,key="pdf_uploader")
+        pdf_docs=st.file_uploader("Upload your PDF Files and Click on the Submit And process Button",accept_multiple_files=True,key="pdf_uploader")
+        # json_docs=st.file_uploader("Upload your PDF Files and Click on the Submit And process Button",accept_multiple_files=True,key="json_uploader")
+
         if st.button("Submit and process",key="process_buttonn") and api_key:
             with st.spinner("Processing..."):
                 raw_text=get_pdf_text(pdf_docs)
+                # raw_text=get_json_text(json_docs)
                 text_chunks=get_text_chunks(raw_text)
                 get_vector_store(text_chunks,api_key)
                 st.success("Done")
@@ -86,4 +95,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
